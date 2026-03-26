3cbc6e2b60458f03357886a17bf78286086a3029
Melvin Sjöstrand <melvin.sjostrand12@gmail.com>
2026-03-26 10:42:29 +0100

    test 2

---
+++ WebApplication1/Controllers/BedAndBreakfast.cs
@@ -0,0 +1,42 @@
+﻿using bedandbreakfast1.Models;
+using bedandbreakfast1.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace bedandbreakfast1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookingController : ControllerBase
+    {
+        private readonly BookingService _service;
+
+        public BookingController(BookingService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("rooms")]
+        public IActionResult GetRooms()
+        {
+            return Ok(_service.GetRooms());
+        }
+
+        [HttpPost("booking")]
+        public IActionResult CreateBooking(int roomId, Guest guest, string checkIn, string checkOut, bool HeadsUp)
+        {
+
+            try
+            {
+                Booking booking = _service.CreateBooking(roomId, guest, checkIn, checkOut, HeadsUp);
+                return StatusCode(201, booking);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }   
+}
+
